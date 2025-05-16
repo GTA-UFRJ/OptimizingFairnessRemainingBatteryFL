0d596908aa6abbec0bc3d74d70e49fa2859e6ddb
guilhermeeec <guilhermepic2016@gmail.com>
2025-05-16 15:19:01 -0300

    WF not working properly

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ class Client:
         gamma_i:float,      # CPU effective capacitance
         ci:int,             # CPU cycles for processing 1 batch
         fi:int,             # CPU clock freq
-        P_down_avg:float,   # avg power consumption
+        P_down_avg:float,   # avg power by the app
         Pi:float,           # charging power,
         ui:float,           # upload time
         di:float,           # download time
@@ -21,51 +21,91 @@ class Client:
 
         self.epsilon_i = Bi*ci*gamma_i*fi*fi # energy per epoch
         self.tau_i = Bi*ci/fi # time per epoch (bacthes * cycles/batch / cycles/sec = sec)
+        self.Ki = self.epsilon_i + self.tau_i * (self.P_down_avg - self.Pi) # energy decreasse per epoch
 
+        print("New client created:")
         print(f"Energy per epoch = {self.epsilon_i} J")
         print(f"Time per epoch = {self.tau_i}s")
+        print(f"Energy decrease per epoch = {self.Ki}s")
+
+    def _setters(self,ri,Ui,Ti,csi):
+        self.ri = ri
+        self.Ui = Ui
+        self.Ti = Ti
+        self.num_epochs = csi-ri
+        self.energy_consumed_training = self.epsilon_i * self.num_epochs
+        self.energy_consumed_base = self.P_down_avg * self.tau_i * self.num_epochs
+        self.energy_recharged = self.Pi * self.tau_i * self.num_epochs
+        self.energy_variation = -self.Ki * self.num_epochs
+        self.Ei = self.Eio - self.Ki * self.num_epochs
+
+    def _sanity_checks(self):
+        assert 2==3
+        assert np.math.log10(self.Ei) == self.Ui + np.math.log10(self.Ni*self.Ki)
+        assert self.energy_variation == self.energy_recharged - self.energy_consumed_training - self.energy_consumed_base
+        assert self.Ei - self.Eio == self.energy_variation 
 
     def compute(
         self, 
         ri:int,  # reduction in num of epochs 
         csi:int  # ceil num of epochs
     )->tuple:    # rtype: (Ui, Ti) = (utility, time)
-        
-        # energy consumption minus charging per epoch
-        Ki = self.epsilon_i + self.tau_i * (self.P_down_avg - self.Pi) 
 
-        Ni = (self.Eio - csi*Ki)/Ki
+        self.Ni = (self.Eio - csi*self.Ki)/self.Ki
+        Ui = np.math.log10(1+ri/self.Ni) # utility
 
-        # utility
-        Ui = np.math.log2(1+ri/Ni)
-        
-        # For sanity check
-        assert np.math.log2(self.Eio - csi*Ki + ri*Ki) == Ui + np.math.log2(Ni*Ki)
-        print(f"Energy before: {self.Eio}")
-        print(f"Energy after: {self.Eio - Ki * (csi-ri)}")
-        print(f"Energy variation: {-Ki * (csi-ri)}")
+        Ti = csi*self.tau_i + self.ui + self.di - ri*self.tau_i # complete round time
 
-        # complete round time
-        Ti = csi*self.tau_i + self.ui + self.di - ri*self.tau_i
+        self._setters(ri,Ui,Ti,csi)
+        self._sanity_checks
 
-        return Ui, Ti
+    def report(self):
+        print(f"Num epochs: {self.num_epochs}")
+        print(f"Reduced num epochs: {self.ri}")
+        print(f"Utility: {self.Ui}")
+        print(f"Time: {self.Ti} s")
+        print(f"Energy before: {self.Eio} J")
+        print(f"Energy consumed by training: {self.energy_consumed_training} J")
+        print(f"Energy consumed base: {self.energy_consumed_base} J")
+        print(f"Energy recharged: {self.energy_recharged} J")
+        print(f"Total energy variation: {self.energy_variation} J")
+        print(f"Energy after: {self.Ei} J")
 
 if __name__ == "__main__":
 
-    Emax = 4000 * 10e-3 * 3600 * 3.7 
-    print(f"Emax = {Emax}")
+    Emax = 3000 * 10e-3 * 3600 * 3.7 
+    print(f"Emax = {Emax} J")
     # mAh * 10e-3 A/mA * 3600 s/h * V = VAs = Ws = J
 
+    print("-----------------------")
     client = Client(
         Eio=0.9*Emax, # 90% SoC battery level
         Bi=100,
         gamma_i=1e-28,
         ci=0.015 * 1e9, # sec/batch * cycles/sec = cycles/batch
         fi=1e9,
-        P_down_avg= 0.0028*Emax, # 0.00345 percent/per second ~ 100% in 10 hours
-        Pi=0.99*10, # charger efficiency = 99%
+        P_down_avg= 0.00000023*Emax, # 0.000023 percent/per second ~ 1% in 12 hours
+        Pi=0.99*(9*1.5), # charger efficiency = 99%
         ui=4*8/10, # sending a yolo nano (4MB) in a LTE 10Mbps upload link
         di=4*8/15 # sending a yolo nano (4MB) in a LTE 15Mbps download link
     )
+    print("Client while charging")
+    client.compute(30,50)
+    client.report()
 
-    print(client.compute(30,50))
+
+    print("-----------------------")
+    client = Client(
+            Eio=0.9*Emax, # 90% SoC battery level
+            Bi=100,
+            gamma_i=1e-28,
+            ci=0.015 * 1e9, # sec/batch * cycles/sec = cycles/batch
+            fi=1e9,
+            P_down_avg= 0.00000023*Emax, # 0.000023 percent/per second ~ 1% in 12 hours
+            Pi=0,
+            ui=4*8/10, # sending a yolo nano (4MB) in a LTE 10Mbps upload link
+            di=4*8/15 # sending a yolo nano (4MB) in a LTE 15Mbps download link
+        )
+    print("Client without charging")
+    client.compute(30,50)
+    client.report()
---
+++ wf_solver.py
@@ -0,0 +1,72 @@
+from client import Client
+from math import floor
+
+class WaterFillingSolver:
+    def __init__(
+        self,
+        clients_list:list,
+        num_min_epochs:int,
+        time_budget:float,
+        thresh:int=1) -> None:
+        
+        self.clients_list = clients_list
+        self.num_clients = len(clients_list)
+        self.time_budget = time_budget        
+        self.e = thresh
+
+        self.csi = self._compute_ceil_num_epochs()
+        self.R =  self.num_clients * self.csi - num_min_epochs
+
+        self._compute_Nis()
+        self.U = (self.R + sum(self.Ni_list)) / self.num_clients
+        self.L = 0 #-self.U
+
+    def _compute_Nis(self):
+        self.Ni_list = [(client.Eio - self.csi * client.Ki) / client.Ki for client in self.clients_list]
+            
+    def _compute_ceil_num_epochs(self):
+        return max([floor(self.time_budget/client.tau_i) for client in self.clients_list])
+
+    def _update_clients(self):
+        self.new_clients_list = []
+        for client, r in zip(self.clients_list, self.r_list):
+            client.compute(r, self.csi) 
+            self.new_clients_list.append(client)
+        self.clients_list = self.new_clients_list
+
+    def _run_iteration(self):
+        alfa = (self.U + self.L)/2
+        self.r_list = []
+        for Ni in self.Ni_list:
+            ri = alfa - Ni
+            if ri > 0 and ri <= self.csi:
+                self.r_list.append(ri)
+            elif ri > 0  and ri > self.csi:
+                self.r_list.append(self.csi)
+            else:
+                self.r_list.append(0)
+
+        print(f"U = {self.U}")
+        print(f"L = {self.L}")
+        print(f"alpha = {alfa}")
+        print(f"Nis = {self.Ni_list}")
+        print(f"rs = {self.r_list}")
+        self._update_clients()
+        self.t_list = [client.Ti for client in self.clients_list]
+        if sum(self.r_list) > self.R: #or self.time_budget > max(self.t_list):
+            self.U = alfa
+        else:
+            self.L = alfa
+
+    def solve(self):
+        self._run_iteration() 
+        while self.R - sum(self.r_list) > self.e and (self.U - self.L) / self.U > 0.01:
+            self._run_iteration()
+        self._report()
+
+    def _report(self):
+        for i, client in enumerate(self.clients_list):
+            print(f"---------------")
+            print(f"Client {i+1}")
+            client.report()
+        
